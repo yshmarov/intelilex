3d32ea22b6c9f73cb39bf6794645aeffe86f73d7
yashm <yshmarov@gmail.com>
2019-02-23 19:46:47 +0000

    update notifications

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -19,7 +19,7 @@ class LeadsController < ApplicationController
 
     respond_to do |format|
       if @lead.save
-        format.html { redirect_to welcome_path, notice: 'Lead was successfully created.' }
+        format.html { redirect_to welcome_path, notice: "You've subscribed! You're the best!" }
         format.json { render :show, status: :created, location: @lead }
       else
         format.html { render :new }
