75928ed034b9187ec8bbd32cbec3906cf2cc865a
yashm <yshmarov@gmail.com>
2019-06-02 09:14:26 +0000

    add additional pages to footer

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
   get 'cancel', to: 'static_pages#cancel'
   get 'completed', to: 'static_pages#completed'
   get 'welcome', to: 'static_pages#welcome'
-  get 'for_press', to: 'static_pages#for_press'
   get 'faq', to: 'static_pages#faq'
+  get 'for_press', to: 'static_pages#for_press'
   get 'features', to: 'static_pages#features'
-  get 'about_us', to: 'static_pages#about_us'
   get 'pricing', to: 'static_pages#pricing'
+  get 'about_us', to: 'static_pages#about_us'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
